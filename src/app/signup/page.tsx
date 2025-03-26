2022e228653fa5520550d940ffd59a77568bd642
Alejou-geeks5g <alejandroalvarez@geek5g.com>
2025-03-26 14:48:11 -0500

    Feat: Apply design pattern

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,6 @@
+"use client"
 import { SignupForm } from "@/components/signup-form"
 
 export default function SignupPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-white p-4">
-      <SignupForm />
-    </div>
-  )
+  return <SignupForm />
 }
