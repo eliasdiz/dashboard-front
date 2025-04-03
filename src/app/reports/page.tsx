1b2ce9d754d55295546fbb3aa439fa2572ad46a0
Alejou-geeks5g <alejandroalvarez@geek5g.com>
2025-04-03 16:54:46 -0500

    Feat: Report page implemented

---
+++ src/app/reports/page.tsx
@@ -0,0 +1,8 @@
+import GoogleMyBusinessDashboard from '@/components/reports/GoogleMyBusinessDashboard';
+import React from 'react'
+
+const page = () => {
+  return <GoogleMyBusinessDashboard />;
+}
+
+export default page
